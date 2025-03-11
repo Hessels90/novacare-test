0777871cf3b2babd2def6cb3cccba2165bd34d52
Hessels90 <Hesselberg90@gmail.com>
2025-03-11 12:33:49 +0100

    added contentful client

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 import styles from "./page.module.css";
+import { createClient } from "contentful";
+
+const client = createClient({
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_API_KEY || "",
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE || "",
+});
+
+const response = await client.getEntries({
+  content_type: "accordion",
+  include: 2,
+});
 
 export default function Home() {
+  console.log(response);
   return (
     <div className={styles.page}>
       <main className={styles.main}>awdawdawd</main>
